50009c5389c6421a2df79127d1c5e11d171c12eb
radwanabil <radwaismail444@gmail.com>
2022-04-04 16:47:29 +0200

    feat: no internet screem

---
+++ lib/src/constants/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color transparent = Color(0x00000000);
+  static LinearGradient background = const LinearGradient(
+    colors: [
+      Color(0xFF5632D0),
+      Color(0x995632D0),
+    ],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+  static const Color primaryColor = Color(0xFFFFFFFF);
+  static const Color primaryColorDark = Color(0xFF25242B);
+  static const Color secondaryColor = Color(0xDCDCDCDC);
+
+}
+
+
+
---
+++ lib/src/widgets/background_container_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+
+class BackgroundContainer extends Container {
+  BackgroundContainer({Key? key, required Widget child})
+      : super(
+
+    key: key,
+    child: child,
+    width: double.infinity,
+    height: double.infinity,
+    decoration: BoxDecoration(gradient: AppColors.background),
+  );
+}
+
