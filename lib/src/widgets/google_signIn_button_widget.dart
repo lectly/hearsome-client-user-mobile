ebdebe2c2bc88d40afcd0d2e02d1153d46f10644
MernaRafik <48571363+MernaRafik@users.noreply.github.com>
2022-04-07 03:07:44 +0200

    feat: created login with google

--- lib/src/widgets/google_signIn_button_widget.dart
+++ lib/src/widgets/google_signIn_button_widget.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:lectly_client_user_mobile/src/constants/colors.dart';
+import '../ui/home_screen.dart';
+import '../utils/authentication.dart';
 
 class GoogleSignInButton extends StatefulWidget {
   const GoogleSignInButton({Key? key}) : super(key: key);
@@ -10,49 +12,75 @@ class GoogleSignInButton extends StatefulWidget {
 }
 
 class _GoogleSignInButtonState extends State<GoogleSignInButton> {
-  //bool _isSigningIn = false;
+  bool _loading = false;
+
+  void _onPressed() async {
+    setState(() {
+      _loading = true;
+    });
+
+    await Authentication.initializeFirebase();
+    User? user = await Authentication.signInWithGoogle(context: context);
+
+    setState(() {
+      _loading = false;
+    });
+
+    if (user != null) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => const HomePage(
+              //user: user,
+              ),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        OutlinedButton(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(AppColors.primaryColor),
-            shape: MaterialStateProperty.all(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: _loading
+          ? const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            )
+          : OutlinedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(AppColors.primaryColor),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              onPressed: _onPressed,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Image(
+                      image: AssetImage("assets/images/google_logo.png"),
+                      height: 24.0,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(
+                        'Continue with Google',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black54,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          onPressed: () async {
-            // // TODO: Add method call to the Google Sign-In authentication
-          },
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
-            child: Row(
-              children: const <Widget>[
-                Image(
-                  image: AssetImage("assets/images/google_logo.png"),
-                  height: 24.0,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 10),
-                  child: Text(
-                    'Continue with Google',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black54,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
