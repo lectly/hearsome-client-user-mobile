f0bc21bae053f2040a90e51374f8e50e9ced49a9
Omar Adel <omarxadel21@gmail.com>
2022-05-11 19:32:18 +0200

    refactor: remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:lectly_client_user_mobile/src/ui/splash/splash.dart';
-
-import 'package:lectly_client_user_mobile/src/ui/profile.dart';
-import 'package:lectly_client_user_mobile/src/ui/no_internet/no_internet.dart';
-import 'package:lectly_client_user_mobile/src/widgets/background_container_widget.dart';
-import 'package:lectly_client_user_mobile/src/widgets/app_bar_widget.dart';
-
-
+import 'package:lectly_client_user_mobile/src/ui/profile/profile.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(
@@ -23,10 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.deepPurple),
-
-
       home: const ProfileScreen(),
-
     );
   }
 }
