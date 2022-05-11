53137479a23c9b24d717f8b4a01ed289bd46e3f2
Omar Adel <omarxadel21@gmail.com>
2022-05-11 19:29:59 +0200

    fix: app bar overflow
    app bar overflowing because it's implemented as a Row widget instead of app bar widget

--- lib/src/widgets/app_bar_widget.dart
+++ lib/src/widgets/app_bar_widget.dart
@@ -1,62 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:lectly_client_user_mobile/src/widgets/dialog_button_widget.dart';
 import '../constants/colors.dart';
-import 'package:lectly_client_user_mobile/src/ui/splash/splash.dart';
 
-class Appbar extends StatefulWidget {
+class Appbar extends StatelessWidget {
   const Appbar({Key? key}) : super(key: key);
 
   @override
-  State<Appbar> createState() => _AppbarState();
-}
-
-class _AppbarState extends State<Appbar> {
-  @override
   Widget build(BuildContext context) {
-    double width = MediaQuery
-        .of(context)
-        .size
-        .width;
-    const SizedBox(height: 10.0,);
-    return Container(
-      color: AppColors.transparent,
-      height: 64,
-      width: width,
-      alignment: Alignment.center,
-
-      child: Row(
-        children: [
-          IconButton(
-            icon: Image.asset('assets/images/back.png'),
-            iconSize: 50,
-            onPressed: () {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => const SplashScreen()));
-            },
-          ),
-          const SizedBox(width: 98.0,),
-           Text(
-            "Profile",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: AppColors.primaryColor,
-              fontFamily: "Cairo",
-              fontSize: 24.0,
-            ),
-          ),
-          const SizedBox(width: 97.0,),
-          IconButton(
-            icon: Image.asset('assets/images/globe.png'),
-            iconSize: 50,
-            onPressed: () {
-              showDialog(context: context, builder: (BuildContext context){
-                return DialogButton();
-              });
-            },
-          ),
-
-        ],
+    return AppBar(
+      backgroundColor: AppColors.transparent,
+      elevation: 0,
+      centerTitle: true,
+      title: const Text(
+        "Profile",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: AppColors.primaryColor,
+          fontFamily: "Cairo",
+          fontSize: 24.0,
+        ),
       ),
+      actions: [
+        IconButton(
+          icon: Image.asset('assets/images/globe.png'),
+          iconSize: 50,
+          onPressed: () {
+            // FIXME: WHEN THE DIALOG IS FIXED, ENABLE THIS PIECE OF CODE
+            // showDialog(
+            //     context: context,
+            //     builder: (BuildContext context) {
+            //       return const DialogButton();
+            //     });
+          },
+        ),
+      ],
+      // TODO: Update back button logic to support closing app
+      //  if end of history tree
+      leading: const BackButton(),
     );
   }
 }
