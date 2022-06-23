66cce70df06d82514ec66a4c58bca5bb6e7141e5
MernaRafik <48571363+MernaRafik@users.noreply.github.com>
2022-06-23 21:28:30 +0200

    fix: dialog bad structure and unclickability of options

--- lib/src/widgets/dialog_button_widget.dart
+++ lib/src/widgets/dialog_button_widget.dart
@@ -2,126 +2,76 @@ import 'package:flutter/material.dart';
 import '../constants/colors.dart';
 import '../utils/localization_service.dart';
 
+var languages = LocalizationService.langs.toList();
+var icons = [
+  "assets/images/american_flag.png",
+  "assets/images/egyptian_flag.png"
+];
 
-String selectedLanguage = LocalizationService.langs.first;
-String value = selectedLanguage;
-bool english = (selectedLanguage == 'English');
 class DialogButton extends StatefulWidget {
   const DialogButton({Key? key}) : super(key: key);
   @override
   _DialogButtonState createState() => _DialogButtonState();
 }
+
 class _DialogButtonState extends State<DialogButton> {
   @override
   Widget build(BuildContext context) {
-    return SimpleDialog(
-
-        backgroundColor: AppColors.transparent,
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-
-                color: AppColors.primaryColorDark,
-
-                borderRadius: BorderRadius.all(Radius.circular(8.0))),
-            height: 48,
-            child: Row(
-                children: [
-                  const SizedBox(
-                    width: 21.0,
-                  ),
-                  english ? Image.asset(
-                    "assets/images/american_flag.png",
-                  ) : Image.asset(
-                    "assets/images/egyptian_flag.png",
-                  ),
-                  SimpleDialogOption(
-                    onPressed: () {
-                      Navigator.pop(context, "English");
-                    },
-                    child: english ? const Text(
-                      'English',
-                      style: TextStyle(
-                        color: AppColors.primaryColor,
-                        fontFamily: "Cairo",
-                        fontSize: 16.0,
-                      ),
-                    ) : const Text(
-                      'العربية',
-                      style: TextStyle(
-                        color: AppColors.primaryColor,
-                        fontFamily: "Cairo",
-                        fontSize: 16.0,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 122.0,
-                  ),
-                  IconButton(
-                    icon: Image.asset('assets/images/arrow.png'),
-                    iconSize: 50,
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ]),
+    return SimpleDialog(backgroundColor: AppColors.transparent, children: [
+      Container(
+        decoration: const BoxDecoration(
+            color: AppColors.primaryColorDark,
+            borderRadius: BorderRadius.all(Radius.circular(8.0))),
+        height: 48,
+        child: SimpleDialogOption(
+          onPressed: () => Navigator.pop(context),
+          child: Row(
+            children: [
+              Image.asset(
+                  icons[LocalizationService.getSelectedLanguageIndex()]),
+              const SizedBox(width: 8),
+              Text(
+                languages[LocalizationService.getSelectedLanguageIndex()],
+                style: const TextStyle(
+                  color: AppColors.primaryColor,
+                  fontFamily: "Cairo",
+                  fontSize: 16.0,
+                ),
+              ),
+              const Spacer(),
+              Image.asset('assets/images/arrow.png'),
+            ],
           ),
-          Container(
-            decoration: const BoxDecoration(
-                color: AppColors.primaryColor,
-                borderRadius: BorderRadius.all(Radius.circular(8.0))),
-            height: 48,
-            child: Row(
-                children: [
-                  const SizedBox(
-                    width: 21.0,
-                  ),
-                  english ? Image.asset(
-                    "assets/images/egyptian_flag.png",
-                  ) : Image.asset(
-                    "assets/images/american_flag.png",
-                  ),
-
-
-                  SimpleDialogOption(
-                    onPressed: () {
-                      Navigator.pop(context, "العربية");
-                      if (english) {
-                        value = 'العربية';
-                        english = !english;
-                      } else {
-                        value = 'English';
-                        english = !english;
-                      }
-                      setState(() {
-                        LocalizationService().changeLocale(value);
-                        //print(value);
-                      });
-                    },
-
-
-                    child: english ? const Text(
-                      'العربية',
-                      style: TextStyle(
-                        color: AppColors.primaryColorDark,
-                        fontFamily: "Cairo",
-                        fontSize: 16.0,
-                      ),
-                    ) : const Text(
-                      'English',
-                      style: TextStyle(
-                        color: AppColors.primaryColorDark,
-                        fontFamily: "Cairo",
-                        fontSize: 16.0,
-                      ),
-                    ),
-                  ),
-
-                ]),
+        ),
+      ),
+      Container(
+        decoration: const BoxDecoration(
+            color: AppColors.primaryColor,
+            borderRadius: BorderRadius.all(Radius.circular(8.0))),
+        height: 48,
+        child: SimpleDialogOption(
+          onPressed: () {
+            LocalizationService().changeLocale(languages[
+                (LocalizationService.getSelectedLanguageIndex() + 1) % 2]);
+            Navigator.pop(context);
+          },
+          child: Row(
+            children: [
+              Image.asset(icons[
+                  (LocalizationService.getSelectedLanguageIndex() + 1) % 2]),
+              const SizedBox(width: 8),
+              Text(
+                languages[
+                    (LocalizationService.getSelectedLanguageIndex() + 1) % 2],
+                style: const TextStyle(
+                  fontFamily: "Cairo",
+                  fontSize: 16.0,
+                ),
+              ),
+            ],
           ),
-        ]);
+        ),
+      ),
+    ]);
   }
 }
-
-
