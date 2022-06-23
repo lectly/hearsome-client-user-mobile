8316fb0822b9fa03714aa2704cd68a016d6a6dd9
MernaRafik <48571363+MernaRafik@users.noreply.github.com>
2022-06-23 21:32:10 +0200

    refactor: remove unused imports

--- lib/src/widgets/drop_down_button_widget.dart
+++ lib/src/widgets/drop_down_button_widget.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:lectly_client_user_mobile/src/constants/colors.dart';
 import 'package:lectly_client_user_mobile/src/utils/localization_service.dart';
 
