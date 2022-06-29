8f0a940b6b8ab6aee54b3d184cdd126043bb5326
MernaRafik <48571363+MernaRafik@users.noreply.github.com>
2022-06-29 02:41:07 +0200

    feat: add card widget for transcribed text

---
+++ lib/src/widgets/card_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:lectly_client_user_mobile/src/constants/colors.dart';
+
+class CardContainer extends FractionallySizedBox {
+  CardContainer({Key? key})
+      : super(
+          key: key,
+          widthFactor: 1,
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 34, vertical: 80),
+            padding:
+                const EdgeInsets.symmetric(vertical: 32.0, horizontal: 16.0),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                color: AppColors.primaryColor,
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 20.0,
+                      color: Colors.black.withOpacity(0.25),
+                      offset: const Offset(0.0, 4.0))
+                ]),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean maximus ipsum sed felis molestie ullamcorper. Donec ante nibh, ullamcorper et faucibus quis, ultricies ut dolor. Proin mattis ex ac turpis dictum, a pretium ante consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor leo at commodo fringilla. Proin non elit odio. In mollis, magna vel consectetur viverra, diam sem mollis odio, id interdum odio magna ac lorem. Sed at quam vitae nisi facilisis pulvinar.",
+                    style: TextStyle(
+                      color: AppColors.primaryColorDark,
+                      height: 1.5,
+                      fontFamily: "Cairo",
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+}
