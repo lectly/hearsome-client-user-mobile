141e43a687224fb05735fdb3ac1e895af0aea82b
Omar Adel <omarxadel21@gmail.com>
2022-06-30 14:19:30 +0200

    fix: widget not being independent button

--- lib/src/ui/main/main.dart
+++ lib/src/ui/main/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lectly_client_user_mobile/src/constants/colors.dart';
-import 'package:lectly_client_user_mobile/src/widgets/background_container_widget.dart';
-import 'package:lectly_client_user_mobile/src/widgets/main_app_bar_widget.dart';
-import 'package:lectly_client_user_mobile/src/widgets/record_button_widget.dart';
+import 'package:lectly_client_user_mobile/src/widgets/appBars/main_app_bar_widget.dart';
+import 'package:lectly_client_user_mobile/src/widgets/buttons/record_button_widget.dart';
+import 'package:lectly_client_user_mobile/src/widgets/containers/background_container_widget.dart';
 import '../../controllers/main.dart';
 
 class MainScreen extends StatefulWidget {
@@ -14,45 +14,33 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainModel extends State<MainScreen> {
-  // TODO: activate when transcription is done
-  // final MainController _controller = Get.put(MainController());
+  final MainController _controller = Get.put(MainController());
 
   @override
   Widget build(BuildContext context) {
     return BackgroundContainer(
       appBar: MainAppBar(),
-      child: Stack(
-        children: [
-          Positioned(
-            child: Align(
-              alignment: Alignment.center,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 137),
-                  Text(
-                    "hold".tr,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      color: AppColors.primaryColor,
-                      fontFamily: "Cairo",
-                      fontSize: 32.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+      child: Center(
+        child: Container(
+            margin: const EdgeInsets.only(top: 32),
+            child: Column(
+              children: [
+                Text(
+                  "hold".tr,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: AppColors.primaryColor,
+                    fontFamily: "Cairo",
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.bold,
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const SizedBox(
-                    height: 128,
-                    width: 128,
-                    child: RecordButton(),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const RecordButton(),
+              ],
+            )),
       ),
     );
   }
